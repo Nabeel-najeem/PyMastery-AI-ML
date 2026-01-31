a55f8e7a3e124e77fb981d9b5a07f50a51ca2f88
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-01-31 13:29:58 +0530

    chamging ide

--- mediapipe_projects/basic.py
+++ mediapipe_projects/basic.py
@@ -1,11 +1,14 @@
-import cv2 
+import cv2
 import mediapipe as mp
-
-cap= cv2.VideoCapture(0)
-
+ 
+h =mp.solutions.hands
+mp_draw = mp.solutions.drawing_utils
+cap = cv2.VideoCapture(0)
 while True :
     success,frame = cap.read()
     frame=cv2.flip(frame,1)
+    rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    result = h.process(rgb_frame)
     
     
     
@@ -15,7 +18,7 @@ while True :
     
     
     
-    cv2.imshow("feed",frame)
+    cv2.imshow("feed",result)
     
     key = cv2.waitKey(1) &0xFF
     if key == ord('q'):
