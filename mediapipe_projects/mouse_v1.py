1602cfa054c91588c784d048bb3ee8a59f0936a4
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-02-01 14:48:46 +0530

    changed smoothen technic to alpha method for better for smoothenin

--- mediapipe_projects/mouse_v1.py
+++ mediapipe_projects/mouse_v1.py
@@ -13,7 +13,9 @@ wh = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 cx4,cy4,cx8,cy8 = 0,0,0,0
 
 px, py = 0,0
-smooh_factor  = 6
+#smooh_factor  = 6
+
+alpha = 0.5
 dead_zone = 5
 
 
@@ -42,29 +44,33 @@ while True :
                     cx8,cy8 = cx, cy
                     #cv2.circle(frame,(cx8,cy8),3,(0,255,0),cv2.FILLED)
 
-                if cx4 !=0 and cx8!=0 :
-                    x_mouse = np.interp(cx8,[0,ww],[0,sw])
-                    y_mouse = np.interp(cy8,[0,wh],[0,sh])
-                    cv2.putText(frame,f"{x_mouse} {y_mouse}",(100,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
-                    scx ,scy = px+(x_mouse-px)/smooh_factor,py+(y_mouse-py)/smooh_factor
-                    movement_dist = math.hypot(x_mouse-px, y_mouse-py)
-                    if movement_dist < dead_zone :
-                        scx, scy = px, py
-                        status = "stable"
-                    else :
-                        px, py = scx,scy
-                        status = "unstable"
-                    cv2.putText(frame,f"{status}", (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),2)
-                    if 100 < scx < 1100 and 100 < scy < 600 :
+            if cx4 !=0 and cx8!=0 :
+                x_mouse = np.interp(cx8,[0,ww],[0,sw])
+                y_mouse = np.interp(cy8,[0,wh],[0,sh])
+                cv2.putText(frame,f"{x_mouse} {y_mouse}",(100,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                #scx ,scy = px+(x_mouse-px)/smooh_factor,py+(y_mouse-py)/smooh_factor
 
-                        cv2.circle(frame,(int(x_mouse),int(y_mouse)),3,(0,0,255),cv2.FILLED)
-                        cv2.circle(frame,(int(scx),int(scy)),3,(0,255,0),cv2.FILLED)
-                        cv2.putText(frame,f"{scx} {scy}",(100,200),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                scx = (alpha*x_mouse) + ((1-alpha)*px)
+                scy = (alpha*y_mouse) + ((1-alpha)*py)
 
-                if cx4 and cx8 :
-                    distance = math.hypot(cx8-cx4, cy8-cy4)
-                    if distance < 20 :
-                        cv2.putText(frame,"click",(1100,200),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+                movement_dist = math.hypot(x_mouse-px, y_mouse-py)
+                if movement_dist < dead_zone :
+                    scx, scy = px, py
+                    status = "stable"
+                else :
+                    px, py = scx,scy
+                    status = "unstable"
+                cv2.putText(frame,f"{status}", (500, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255),2)
+                if 100 < scx < 1100 and 100 < scy < 600 :
+
+                    cv2.circle(frame,(int(x_mouse),int(y_mouse)),3,(0,0,255),cv2.FILLED)
+                    cv2.circle(frame,(int(scx),int(scy)),3,(0,255,0),cv2.FILLED)
+                    cv2.putText(frame,f"{scx} {scy}",(100,200),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
+
+            if cx4 and cx8 :
+                distance = math.hypot(cx8-cx4, cy8-cy4)
+                if distance < 20 :
+                    cv2.putText(frame,"click",(1100,200),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),2)
 
     cv2.rectangle(frame,(100,100),(1100,600),(0,0,255),2)
 
@@ -73,3 +79,5 @@ while True :
     if cv2.waitKey(1)&0xFF == ord('q') :
         break
 
+cap.release()
+cv2.destroyAllWindows()
