cab2aefb3de5fc1f7777cc339dbcb7718fecee69
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-02-09 22:19:13 +0530

    used new modules to find left and right pupils and draw points over that

--- mediapipe_projects/main.py
+++ mediapipe_projects/main.py
@@ -3,10 +3,7 @@ import face_engine as fe
 
 cap = cv2.VideoCapture(0)
 
-
-
 detector = fe.face_detector()
-right_eye = fe.face_detector()
 
 while True:
     success,frame = cap.read()
@@ -15,12 +12,16 @@ while True:
     frame = cv2.flip(frame,1)
 
     landmarks = detector.find_face_data(frame)
-    x,y,z = right_eye.find_pupils(frame)
-    if x :
-        cv2.circle(frame, (x, y), 3, (0, 0, 255), cv2.FILLED)
-        #cv2.putText(frame,f"{eye}",(100,200),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
+
+    xr,yr = detector.x_y_landmarks(frame=frame,id=468)
+    xl,yl = detector.x_y_landmarks(frame=frame,id=473)
+    if xr and yr is not None:
+        cv2.circle(frame, (xr, yr), 3, (0, 0, 255), cv2.FILLED)
+        cv2.circle(frame, (xl, yl), 3, (0, 0, 255), cv2.FILLED)
+
+
     length = len(landmarks)
-    if not length == 0 :
+    if  length != 0 :
         cv2.putText(frame,f"{length}",(100,100),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255))
 
 
@@ -33,3 +34,5 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+
+
