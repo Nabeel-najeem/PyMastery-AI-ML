01eba6c4535cae028d6f1f9a47f8fe27eca35c79
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-02-11 19:17:30 +0530

    impleted click logic and smothen logic to main file

--- mediapipe_projects/main.py
+++ mediapipe_projects/main.py
@@ -1,63 +1,66 @@
-import math
-
 import cv2
+import pyautogui as pag
+
 import face_engine as fe
+import signal_processor as sp
 
 cap = cv2.VideoCapture(0)
 
+ww = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+wh = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+screen_w, screen_h = pag.size()
+
+
 detector = fe.face_detector()
+smooth_r = sp.smoother()
+smooth_l = sp.smoother()
 
 while True:
     success,frame = cap.read()
     if not success:
         break
     frame = cv2.flip(frame,1)
-
     landmarks = detector.find_face_data(frame)
 
 
     #pupils
-    xr,yr = detector.x_y_landmarks(frame=frame,id=468)
-    xl,yl = detector.x_y_landmarks(frame=frame,id=473)
-    if xr and yr is not None:
+    xl,yl = detector.x_y_landmarks(frame=frame,id=468)
+    xr,yr = detector.x_y_landmarks(frame=frame,id=473)
+    if xr is not None and yr is not None:
         cv2.circle(frame, (xr, yr), 3, (0, 0, 255), cv2.FILLED)
         cv2.circle(frame, (xl, yl), 3, (0, 0, 255), cv2.FILLED)
+
+    sxr,syr,dar,sr = smooth_r.smooth(xr,yr)
+    sxl,syl,dal,sl = smooth_l.smooth(xl,yl)
+    if not None in [sxr,syr,dar,sxl,syl,dal]:
+        cv2.circle(frame, (sxr, syr), 3, (0, 255, 255), cv2.FILLED)
+        cv2.circle(frame, (sxl, syl), 3, (0, 255, 255), cv2.FILLED)
+        if sr == True and sl == True :
+            cv2.putText(frame, f"stable", (900, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        if sr == False and sl == False :
+            cv2.putText(frame, f"unstable", (900, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
     #dynamic treshold
     treshold_click = detector.dynamic_treshold(frame)
     cv2.putText(frame, f"treshold-{treshold_click}", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1,(0, 0, 255), 2)
 
-    #rightt eye lice
-    retx ,rety = detector.x_y_landmarks(frame=frame,id=386)
-    relx, rely = detector.x_y_landmarks(frame=frame,id=374)
-    if retx and relx is not None:
-        right_eye_distance = math.hypot(retx-relx,rety-rely)
-        cv2.putText(frame, f"distance-{right_eye_distance}", (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.circle(frame, (retx, rety), 1, (255, 0, 255), cv2.FILLED)
-        cv2.circle(frame, (relx, rely), 1, (255, 0, 255), cv2.FILLED)
-
-    #left eye lice
-    letx ,lety = detector.x_y_landmarks(frame=frame,id=159)
-    lelx, lely = detector.x_y_landmarks(frame=frame,id=145)
-    if retx and relx is not None:
-        left_eye_distance = math.hypot(letx-lelx,lety-lely)
-        cv2.putText(frame, f"distance-{left_eye_distance}", (100, 140), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.circle(frame, (letx, lety), 1, (255, 0, 255), cv2.FILLED)
-        cv2.circle(frame, (lelx, lely), 1, (255, 0, 255), cv2.FILLED)
 
     #click logic
-    if right_eye_distance and left_eye_distance :
-        avg_distance = (right_eye_distance + left_eye_distance)/2
-        if avg_distance < treshold_click :
-            cv2.putText(frame, f"click", (1000, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    left_eye = detector.click(frame,159,145)
+    right_eye = detector.click(frame,386,374)
+    if left_eye is not None and right_eye is not None:
+        cv2.putText(frame,f"right - {right_eye} and left - {left_eye}", (300, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        avg_dis = (left_eye+right_eye)/2
+        if  avg_dis  < treshold_click :
+            cv2.putText(frame,"click", (500, 500), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
 
     cv2.imshow('frame',frame)
     key = cv2.waitKey(1)
-
     if key == ord('q'):
         break
 
+
 cap.release()
 cv2.destroyAllWindows()
-
 
