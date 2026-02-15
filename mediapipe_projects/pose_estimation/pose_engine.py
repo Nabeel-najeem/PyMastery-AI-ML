36db005584f8110a833865f5b43b916c379f1066
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-02-15 12:27:15 +0530

    added angle calculation module

--- mediapipe_projects/pose_estimation/pose_engine.py
+++ mediapipe_projects/pose_estimation/pose_engine.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 import mediapipe as mp
 
 class possDetector:
@@ -41,7 +42,22 @@ class possDetector:
 
             if left_shurg_dist < 0.3 and right_shurg_dist < 0.3 :
                 return True
-
         return False
+
+    def get_angle(self,landmarks,p1,p2,p3):
+        if landmarks != None:
+            left_sholder = landmarks[11]
+            x1, y1 = landmarks[p1].x, landmarks[p1].y
+            x2, y2 = landmarks[p2].x, landmarks[p2].y
+            x3, y3 = landmarks[p3].x, landmarks[p3].y
+
+            angle = math.degrees(math.atan2(y3-y2,x3-x2)-
+                                 math.atan2(y1-y2,x1-x2))
+
+            angle =abs(angle)
+            if angle > 180 :
+                angle = 360-angle
+            return angle
+        return None
 
 
