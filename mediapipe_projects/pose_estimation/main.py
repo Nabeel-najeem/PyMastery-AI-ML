b9a7d54f796a6adbe935411dd8f14fde50f4d9a6
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-02-15 12:27:54 +0530

    implement the angle find module in main

--- mediapipe_projects/pose_estimation/main.py
+++ mediapipe_projects/pose_estimation/main.py
@@ -21,6 +21,10 @@ while True:
     if is_shurg == True:
         cv2.putText(frame, f"shurg detected", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
+    angle = pose.get_angle(pose_lms,11,13,15)
+    if angle != None:
+        cv2.putText(frame, f"{angle}", (100, 230), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
     cv2.imshow('frame',frame)
 
     key = cv2.waitKey(1) & 0xFF
