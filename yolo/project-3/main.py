6bff19b49bf183ee2ca74db19e20811348c1b5a4
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-02-25 16:18:19 +0530

    added velocity with track_id for unique objects

--- yolo/project-3/main.py
+++ yolo/project-3/main.py
@@ -1,58 +1,60 @@
 import cv2
+import time
 from ultralytics import YOLO
-
 
 model = YOLO("yolov8n.pt")
 
-real_width = {
-    "cell phone" : 7.5,
-    "bottle" : 7.0
-}
+real_width = {"cell phone": 7.5,
+              "bottle": 7.0,
+              "person" : 36,
+              }
 focal_length = 650
-
 cap = cv2.VideoCapture(0)
 
-while True :
-    sucess,frame = cap.read()
-    if not sucess : 
-        break
-    frame = cv2.flip(frame,1)
-    
-    result = model.track(frame,persist=True,device = 0, verbose = False)
-    
-    frame_counts = {}
-    
-    for r in result :
-        boxes = r.boxes
-        for box in boxes :
-            cls_id = int(box.cls[0])
-            label = r.names[cls_id]
-            
-            frame_counts[label]=frame_counts.get(label,0)+1
-            
-            if label in real_width :
-                
-                w_pixel = float(box.xywh[0][2])
-                
-                distance = (real_width[label]*focal_length)/w_pixel
-                
-                x1,y1,x2,y2 = map(int,box.xyxy[0])
-                cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2)
-                cv2.putText(frame,f"distance : {distance: .1f}cm",(x1,y1-10),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,0.6,(0,255,0),2)
 
-                y_offset = 30
-                for obj_name,count in frame_counts.items():
-                    text = f"Total {obj_name}s : {count}"
-                    cv2.putText(frame,text,(20,y_offset),cv2.FONT_HERSHEY_SCRIPT_SIMPLEX,0.6,(0,255,0),2)
-                    y_offset+=30
+track_history = {}
+
+while True:
+    success, frame = cap.read()
+    if not success: break
+    frame = cv2.flip(frame, 1)
+    current_time = time.time()
     
+    results = model.track(frame, persist=True, device=0, verbose=False)
     
-    cv2.imshow("frame",frame)
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
+    for r in results:
+        if r.boxes.id is not None:
+            boxes = r.boxes.xywh.cpu()
+            track_ids = r.boxes.id.int().cpu().tolist()
+            cls_ids = r.boxes.cls.int().cpu().tolist()
+
+            for box, track_id, cls_id in zip(boxes, track_ids, cls_ids):
+                label = r.names[cls_id]
+                
+                if label in real_width:
+                    w_pixel = float(box[2])
+                    distance = (real_width[label] * focal_length) / w_pixel
+                    
+                    speed = 0
+                    if track_id in track_history:
+                        prev_dist, prev_time = track_history[track_id]
+                        
+                        delta_d = abs(prev_dist - distance)
+                        delta_t = current_time - prev_time
+                        
+                        if delta_t > 0:
+                            speed = delta_d / delta_t 
+                    
+                    track_history[track_id] = (distance, current_time)
+
+                    x, y, w, h = box
+                    x1, y1 = int(x - w/2), int(y - h/2)
+                    cv2.putText(frame, f"Dist: {distance:.1f}cm", (x1, y1-10), 0, 0.5, (0,255,0), 2)
+                    cv2.putText(frame, f"Speed: {speed:.1f}cm/s", (x1, y1-30), 0, 0.5, (255,0,0), 2)
+                    cv2.rectangle(frame, (x1, y1), (int(x+w/2), int(y+h/2)), (0,255,0), 2)
+
+    cv2.imshow("Velocity Engine - Day 41", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
+
 cap.release()
 cv2.destroyAllWindows()
-    
-    
