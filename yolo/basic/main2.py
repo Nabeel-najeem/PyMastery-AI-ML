4ff0520bbfed27cec2aedb5475314512a6daa29d
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-03-03 13:20:01 +0530

    added multi object counting with displaying list type

--- yolo/basic/main2.py
+++ yolo/basic/main2.py
@@ -7,7 +7,9 @@ cap = cv2.VideoCapture(0)
 
 
 while True :
-    person_count = 0    
+    count = {"person" : 0,
+             "cell phone" : 0 }  
+    y_value = 25  
     success,frame = cap.read()
     if success == True :
         frame = cv2.flip(frame,1)
@@ -22,18 +24,30 @@ while True :
                 
                 if cls_id == 0 :
                     color =(0,0,255)
+                    object = 'person'
                     label = f"Target : person {conf:.2f}"
-                    person_count += 1
+                    #count["person"] +=1
                 elif cls_id == 67 :
+                    object = 'cell phone'
                     color =(0,255,0)
                     label = f"Target : phone {conf:.2f}"
                 else :
                     continue
                 
                 
-                cv2.putText(frame,f"total person present : {person_count}",(25,25), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 2)
+                
+                count[object] += 1
                 cv2.putText(frame,label, ((x1-10),(y1-10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 2)
                 cv2.rectangle(frame,(x1,y1),(x2,y2),color,3)
+                
+                
+        for key , val in count.items() :
+            if val > 0 :
+                text = f"Total {key} : {val}"
+                cv2.putText(frame,text,(25,y_value), cv2.FONT_HERSHEY_SIMPLEX, 0.5,color, 2)
+                y_value +=25    
+                
+                
                 
                 
             
@@ -47,4 +61,6 @@ while True :
     
 cap.release()
 cv2.destroyAllWindows()
+
+
     
