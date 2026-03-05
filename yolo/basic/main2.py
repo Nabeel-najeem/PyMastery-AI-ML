376a260a6ed4cd0c40949b411484c30f52638761
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-03-05 21:32:01 +0530

    implemented ROI to the  system

--- yolo/basic/main2.py
+++ yolo/basic/main2.py
@@ -1,24 +1,27 @@
 import cv2
 from ultralytics import YOLO
 
-# 1. Load the model - Moving to GPU for better FPS on your RTX 2050
+
 model = YOLO("yolov8n.pt").to('cuda') 
 
 cap = cv2.VideoCapture(0)
 
 while True:
+    
     success, frame = cap.read()
     if not success:
         break
     
     frame = cv2.flip(frame, 1)
-    h, w, _ = frame.shape  # Get dimensions for the border
+    h, w, _ = frame.shape 
     
-    # Reset counts for every frame
+    gate_line_x = w//3
+    
+    cv2.line(frame,(gate_line_x,0),(gate_line_x,h),(255,255,0),2)
+    
     count = {"person": 0, "cell phone": 0} 
     result = model(frame, conf=0.25, iou=0.01, verbose=False)
     
-    # --- DETECTION LOOP ---
     for r in result:
         for box in r.boxes:
             cls_id = int(box.cls[0])
@@ -27,8 +30,14 @@ while True:
             
             if cls_id == 0:
                 obj_name, color = "person", (0, 0, 255) # Red
+                cx = (x1+x2)//2
+                if cx < gate_line_x :
+                    cv2.putText(frame, f"entered resyricted area !!", (500, 400), 
+                                cv2.FONT_HERSHEY_SIMPLEX,0.5, (0,0,255), 6)
+                        
+                
             elif cls_id == 67:
-                obj_name, color = "cell phone", (0, 255, 0) # Green
+                obj_name, color = "cell phone", (0, 255, 0) 
             else:
                 continue
             
@@ -37,21 +46,16 @@ while True:
             cv2.putText(frame, f"{obj_name} {conf:.2f}", (x1, y1 - 10), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
-    # --- THE ALERT LOGIC ---
-    # Trigger alarm if more than 2 people are detected
     alert_mode = count["person"] > 2
     
     if alert_mode:
-        # Draw a thick RED border around the whole screen
         cv2.rectangle(frame, (0, 0), (w, h), (0, 0, 255), 30)
         cv2.putText(frame, "!!! ALERT: CROWD !!!", (w//4, h-50), 
                     cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 4)
 
-    # --- UI DASHBOARD ---
     y_val = 40
     for key, val in count.items():
         if val > 0:
-            # If alert is on, text turns YELLOW to stand out
             txt_color = (0, 255, 255) if alert_mode else (255, 255, 255)
             cv2.putText(frame, f"Total {key}: {val}", (20, y_val), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.8, txt_color, 2)
