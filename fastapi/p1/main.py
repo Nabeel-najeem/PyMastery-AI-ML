a1e11137e06480a6eee2b67b81bd95a9c69a7986
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-04-01 20:49:05 +0530

    hellow world to fast api

---
+++ fastapi/p1/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message" : "Hello Hexer. Your  API is alive."}
+
+@app.get("/status")
+async def sattus():
+    return {
+        "Stusent" : "Nabeel",
+        "Colleeg" : "MUsaliar",
+        "Status" : "Ready for Ai"
+        }
