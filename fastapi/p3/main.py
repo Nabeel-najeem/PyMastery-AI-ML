1ecd53aac3ebf20f6ba2f8e61bcd527d8266d18c
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-04-05 20:16:48 +0530

    added cpu frequency and disk usage to system moniter

--- fastapi/p3/main.py
+++ fastapi/p3/main.py
@@ -11,12 +11,16 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     ram_usage = f"{psutil.virtual_memory().percent}%"
+    cpu_frequency = psutil.cpu_freq().current
+    Disk_usage = f"{psutil.disk_usage('/').percent}%"
     return templates.TemplateResponse(
         request=request,
         name="index.html",
         context={
             "user_name": "Nabeel",
             "college": "Musaliar college",
-            "ram_usage": ram_usage
+            "ram_usage": ram_usage,
+            "cpu_frequency": cpu_frequency,
+            "Disk_usage": Disk_usage
         }
     )
