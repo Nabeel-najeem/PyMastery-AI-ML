49523d1a1515270f3debd4cf221a2c6148093c15
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-04-06 20:58:09 +0530

    added network io

--- fastapi/p3/main.py
+++ fastapi/p3/main.py
@@ -13,6 +13,12 @@ async def home(request: Request):
     ram_usage = f"{psutil.virtual_memory().percent}%"
     cpu_frequency = psutil.cpu_freq().current
     Disk_usage = f"{psutil.disk_usage('/').percent}%"
+    
+    net_io = psutil.net_io_counters()
+    net_sent = f"{net_io.bytes_sent / (1024**2):.2f} MB"
+    net_recv = f"{net_io.bytes_recv / (1024**2):.2f} MB"
+    
+    
     return templates.TemplateResponse(
         request=request,
         name="index.html",
@@ -21,6 +27,8 @@ async def home(request: Request):
             "college": "Musaliar college",
             "ram_usage": ram_usage,
             "cpu_frequency": cpu_frequency,
-            "Disk_usage": Disk_usage
+            "Disk_usage": Disk_usage,
+            "net_sent": net_sent,
+            "net_recv": net_recv
         }
     )
