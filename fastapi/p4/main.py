cafc72dc32f2d4b78f99adaaeeea3cc882985481
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-04-14 22:03:09 +0530

    added edit product and delete product based id

--- fastapi/p4/main.py
+++ fastapi/p4/main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
-from model import products as pd
+from model import product
+from database import Session
 
 
 app = FastAPI()
 
-product = [
-    pd(id=1,product_name="phone",description= "budget phone",price = 20000.0,quantity=10),
-    pd(id=2,product_name="laptop",description= "budget laptop",price = 45000.0,quantity=12),
-    pd(id=3,product_name="fan",description= "budget fan",price = 7000.0,quantity=9),
-    pd(id=4,product_name="car",description= "budget car",price = 200000.0,quantity=7),
+products = [
+    product(id=1,product_name="phone",description= "budget phone",price = 20000.0,quantity=10),
+    product(id=2,product_name="laptop",description= "budget laptop",price = 45000.0,quantity=12),
+    product(id=3,product_name="fan",description= "budget fan",price = 7000.0,quantity=9),
+    product(id=4,product_name="car",description= "budget car",price = 200000.0,quantity=7),
     
 ]
 
@@ -16,14 +17,37 @@ product = [
 def greet():
     return "Welcome to my shope"
 
-@app.get("/all_products")
+@app.get("/products")
 def all_products():
-    return product
+    db = Session()
+    db.query()
+    return products
 
 @app.get("/product/{id}")
 def get_product_id(id : int):
-    for item in product :
-        if item.id == id :
-            return item
+    for product in products :
+        if product.id == id :
+            return product
+    return "product not found"
         
+@app.post("/product")
+def add_product(product : product):
+    products.append(product)
+    return product
+
+@app.put("/product")
+def update_product(id : int , product : product):
+    for i in range(len(products)):
+        if products[i].id == id :
+            products[i] = product
+            return "product added sussefully"
+        
+    return "No product found"
+
+@app.delete("/product")
+def del_product(id :int):
+    for i in range(len(products)):
+        if products[i].id == id :
+            del products[i]
+            return "product deleted sucesfully"
     return "product not found"
