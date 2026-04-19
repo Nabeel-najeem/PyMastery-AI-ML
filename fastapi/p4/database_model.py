a0010e9e74c8b186ed443580da0adc464aead534
Nabeel-najeem <nabeelnajeem6680@gmail.com>
2026-04-19 21:26:52 +0530

    comments important in file and some cleanup

--- fastapi/p4/database_model.py
+++ fastapi/p4/database_model.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class product(Base):
-    
+    #datbase table name
     __tablename__ = "product"
     id = Column(Integer,primary_key = True, index = True)
     product_name = Column(String)
